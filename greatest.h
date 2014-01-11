dd9be8ac791a39dd5a3643592f77e390dd4a8a3c
Edward <eschemb1@jhu.edu>
2014-01-10 23:30:54 -0500

    Cleaned makefile and greatest.h

---
+++ greatest.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2011 Scott Vokes <vokes.s@gmail.com>
+ * 				 2014 Edward Schembor <edschembor@gmail.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * 
+ * Based on the "greatest" C library by Scott Vokes
+ */
+
+#ifndef GREATEST_H
+#define GREATEST_H
+
+/* A unit testing system for C++, contained in 1 file.
+ * It doesn't use dynamic allocation or depend on anything
+ * beyond ANSI C89. */
