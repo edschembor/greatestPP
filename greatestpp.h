998bbd8c1cd66bbe63481c6535f99ee1b798f814
Edward <eschemb1@jhu.edu>
2014-01-14 15:21:31 -0500

    Finished types

--- greatestpp.h
+++ greatestpp.h
@@ -125,3 +125,57 @@ typedef enum
 	FLAG_FIRST_FAIL = 0x02,
 	FLAG_LIST_ONLY = 0x04
 } FLAG;
+
+typedef struct run_info
+{
+	unsigned int flags;
+	unsigned int tests_run;
+	
+	//Overall counts
+	unsigned int passed;
+	unsigned int failed;
+	unsigned int skipped;
+	
+	//Currently running test suite
+	suite_info suite;
+	
+	//Data on the most recent failure
+	const char *fail_file;
+	unsigned int fail_line;
+	const char *msg;
+	
+	//Current setup/teardown hooks and userdata
+	setup_cb *setup;
+	void *setup_udata;
+	teardown_cb *teardown;
+	void *teardown_udata;
+	
+	//Formatting info
+	unsigned int col;
+	unsigned int width;
+	
+	//Only run a specific suite or test
+	char *suite_filter;
+	char *test_filter;
+	
+	//Overall timers
+	clock_t begin;
+	clock_t end;
+} run_info;
+
+/*Global var for the current testing context.
+ * Initialized by MAIN_DEFS(). */
+extern run_info info;
+
+/*****************************
+ *    Exported Functions     *
+ *****************************/
+ 
+ void do_pass( const char *name );
+void do_fail( const char *name );
+void do_skip( const char *name );
+int pre_test( const char *name );
+void post_test( const char *name, int res );
+void usage( const char *name );
+void SET_SETUP_CB( setup_cb *cb, void *udata );
+void SET_TEARDOWN_CB( teardown_cb *cb, void *udata );
